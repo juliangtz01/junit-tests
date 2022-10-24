d6e4d1a1f39e1fe10c673aacab1be155d7e3637d
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-24 12:49:08 -0500

    created the Student clss and the StudentTest class, created the construtor along with the getters and methods in the StudentTest class

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class StudentTest
+{
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+    //The Student class should have a constructor that sets both the name and id property, it initializes the grades property as an empty ArrayList.
+    public StudentTest(String name, long id)
+    {
+        this.name = name;
+        this.id = id;
+
+        this.grades = new ArrayList<>();
+    }
+
+    //The Student class should have the following methods:
+
+    // returns the student's name
+    public String getName()
+    {
+        return name;
+    }
+
+    // returns the student's id
+    public long getId()
+    {
+        return id;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade)
+    {
+        grades.add(grade);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades()
+    {
+        return grades;
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage()
+    {
+        int sum = 0;
+
+        for(int grade : grades)
+        {
+            sum += grade;
+        }
+
+        return (double)sum / grades.size();
+    }
+}
