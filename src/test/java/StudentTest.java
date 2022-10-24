e11a6eb079299c5add47d409adf098611365cc4c
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-24 13:14:02 -0500

    added a main in StudentTest to test my Student class methods

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,14 @@
 public class StudentTest
 {
+    public static void main(String[] args)
+    {
+        Student julian = new Student("Julian", 01);
 
+        julian.addGrade(80);
+        julian.addGrade(90);
+        julian.addGrade(80);
+        julian.addGrade(70);
+
+        System.out.printf("Student %s has a grade average of %.0f.", julian.getName(), julian.getGradeAverage());
+    }
 }
